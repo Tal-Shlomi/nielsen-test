3ffaaa313dfa17c9206a7b3c94636d7f221e8991
talshlomi <tal.shlomi@spotinst.com>
2019-08-04 09:30:08 +0300

    init

--- ansible/files/twitter_streamer.py
+++ ansible/files/twitter_streamer.py
@@ -34,21 +34,25 @@ class StdOutListener(StreamListener):
         t = json.loads(data)
 
         # Pull important data from the tweet to store in the database.
+        tweet_id = t['id_str']  # The Tweet ID from Twitter in string format
         username = t['user']['screen_name']  # The username of the Tweet author
+        followers = t['user']['followers_count']  # The number of followers the Tweet author has
         text = t['text']  # The entire body of the Tweet
+        hashtags = t['entities']['hashtags']  # Any hashtags used in the Tweet
+        dt = t['created_at']  # The timestamp of when the Tweet was created
+        language = t['lang']  # The language of the Tweet
 
         # Convert the timestamp string given by Twitter to a date object called "created". This is more easily manipulated in MongoDB.
-
-        #created = datetime.datetime.strptime(dt, '%a %b %d %H:%M:%S +0000 %Y')
+        created = datetime.datetime.strptime(dt, '%a %b %d %H:%M:%S +0000 %Y')
 
         # Load all of the extracted Tweet data into the variable "tweet" that will be stored into the database
-        tweet = {'username':username, 'text':text}
+        tweet = {'id':tweet_id, 'username':username, 'followers':followers, 'text':text, 'hashtags':hashtags, 'language':language, 'created':created}
 
         # Save the refined Tweet data to MongoDB
         collection.save(tweet)
 
         # Optional - Print the username and text of each Tweet to your console in realtime as they are pulled from the stream
-        #print username + ':' + ' ' + text
+        print username + ':' + ' ' + text
         return True
 
     # Prints the reason for an error to your console
